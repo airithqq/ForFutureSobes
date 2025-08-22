e9548c3f8119d0db9b2227b0cafd238687674ef2
airithqq <benediykdmytro@gmail.com>
2025-08-22 22:36:38 +0300

    Initial commit: created new repository

---
+++ ForFutureSobes/Data/ForFutureSobesDbContext.cs
@@ -0,0 +1,21 @@
+﻿
+using ForFutureSobes.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ForFutureSobes.Data
+{
+    public class ForFutureSobesDbContext: DbContext
+    {
+        public ForFutureSobesDbContext(DbContextOptions dbContextOptions) :base(dbContextOptions)
+        { 
+        }
+
+        public DbSet<Aspnet> Aspnet { get; set; }
+
+        public DbSet<EntityFW> Efw { get; set; }
+
+        public DbSet<Program_language> Pl { get; set; }
+
+        public DbSet<Sqlth> Sql_th { get; set; }
+    }
+}
---
+++ ForFutureSobes/Domain/Sqlth.cs
@@ -0,0 +1,13 @@
+﻿namespace ForFutureSobes.Domain
+{
+    public class Sqlth
+    {
+        public int Id { get; set; }
+
+        public string Task { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+        public char Priority { get; set; }
+    }
+}
